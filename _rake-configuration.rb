55d9d2d90cfc6df25f3a0372234d6e24b6e44105
Jonathan Stegall <jonathan@jonathanstegall.com>
2014-05-22 00:06:31 -0400

    Delete _rake-configuration.rb

--- _rake-configuration.rb
+++
@@ -1,4 +0,0 @@
-# where the Site is going to live
-$deploy_url = "http://jonathanstegall.com"
-# the directory on the server (rsync like syntax)
-$deploy_dir = "jonathanstegall@jonathanstegall.com:/home/jonathanstegall/jonathanstegall.com/jekyll"

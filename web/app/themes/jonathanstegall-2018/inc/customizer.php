571358c09aada8a93331677a6415df72c8bce5ba
Jonathan Stegall <jstegall@minnpost.com>
2018-09-16 14:45:51 -0500

    setup assets directory

--- web/app/themes/jonathanstegall-2018/inc/customizer.php
+++ web/app/themes/jonathanstegall-2018/inc/customizer.php
@@ -50,6 +50,6 @@ function jonathanstegall_2018_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function jonathanstegall_2018_customize_preview_js() {
-	wp_enqueue_script( 'jonathanstegall-2018-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'jonathanstegall-2018-customizer', get_template_directory_uri() . 'assets/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'jonathanstegall_2018_customize_preview_js' );
